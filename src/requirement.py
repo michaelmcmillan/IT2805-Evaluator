7a9f1172f66c6c40b7f7d750e9b1d0dd8d200e92
Michael McMillan <email@michaelmcmillan.net>
2015-10-31 18:37:04 +0100

    remove fulfilled and penalty from requirement

--- src/requirement.py
+++ src/requirement.py
@@ -4,16 +4,8 @@ class Requirement(object):
     
     def __init__(self):
         self.text = "Unknown requirement."
-        self.fulfilled = False
         self._counts = Score(0)
 
-    @property
-    def penalty(self):
-        if not self.fulfilled:
-            return self._counts._points
-        else:
-            return 0
-    
     @property
     def counts(self):
         return self._counts._points
--- test/requirement_test.py
+++ test/requirement_test.py
@@ -6,10 +6,6 @@ from src.requirement import Requirement
 
 class TestRequirement(unittest.TestCase):
 
-    def test_requirement_is_by_default_not_met(self):
-        requirement = Requirement()
-        assert requirement.fulfilled == False
-
     def test_requirement_can_be_set_to_be_fulfilled(self):
         requirement = Requirement()
         requirement.fulfilled = True
@@ -26,15 +22,3 @@ class TestRequirement(unittest.TestCase):
     def test_requirement_counts_zero_as_default(self):
         requirement = Requirement()
         assert requirement.counts == 0
-
-    def test_requirement_has_score_penalty_when_not_fulfilled(self):
-        requirement = Requirement()
-        requirement.counts = 5
-        requirement.fulfilled = False
-        assert requirement.penalty == 5
-
-    def test_requirement_does_not_have_score_penalty_when_fulfilled(self):
-        requirement = Requirement()
-        requirement.counts = 15
-        requirement.fulfilled = True 
-        assert requirement.penalty == 0
